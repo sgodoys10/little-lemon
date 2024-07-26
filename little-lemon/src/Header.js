97940705645f5fa648cde7382f1646efcc73fc26
sgodoys10 <sgodoys10@gmail.com>
2024-07-25 18:50:00 -0700

    third commit

---
+++ little-lemon/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Logo from "./assets/Logo.png"
+
+
+export function Header (){
+    return (
+    <header>
+        <img src={Logo} alt="Logo" className="header-logo"></img>
+    </header>
+)
+}
+
+
+
---
+++ little-lemon/src/Nav.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export function Nav (){
+    return (
+<nav>
+    <ul>
+        <li>
+            <a href="/home">Home</a>
+        </li>
+        <li>
+            <a href="/about">About</a>
+        </li>
+        <li>
+            <a href="/menu">Menu</a>
+        </li>
+        <li>
+            <a href="/reserve">Reserve a table</a>
+        </li>
+        <li>
+            <a href="/Order">Order Online</a>
+        </li>
+    </ul>
+</nav>
+)
+}
+
